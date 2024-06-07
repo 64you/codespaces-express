97741d7eddf0b0a88a0cba6053f301a5bb55318b
64you <142720062+64you@users.noreply.github.com>
2024-06-07 04:51:14 +0000

    public/images下にtest.pngを追加、静的ファイル

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const path = require('path');
 
+const loggerMiddleware = function(req, res, next) {
+  console.log(`[${new Date()}] ${req.method} ${req.url}`);
+  next();
+};
 
+app.use(loggerMiddleware);
+
+// 静的ファイルの提供とキャッシュの有効化
+app.use(express.static('public', { maxAge: 86400000 }));
+
+// test-imageリクエスト
+app.get('/test-image', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/images/test.png'));
+});
 
 // GETリクエスト
 app.get('/', (req, res) => {
@@ -34,6 +47,6 @@ app.delete('/:id', (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
